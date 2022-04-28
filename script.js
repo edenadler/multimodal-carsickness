caeb4ba71384eca010240cd89b92be8c047162f9
Eden Adler <edenadler@C02G915GMD6P.tld>
2022-04-28 12:25:38 -0400

    implementation of z axis

--- script.js
+++ script.js
@@ -24,66 +24,83 @@ const setupCamera = () => {
     });
 };
 
-const handleOrientation = () => {
-  console.log("hi!!!")
-  window.addEventListener('deviceorientation',(event) => {
-    // Expose each orientation angle in a more readable way
-    rotation_degrees = event.alpha;
-    frontToBack_degrees = event.beta;
-    leftToRight_degrees = event.gamma;
+// const handleOrientation = () => {
+//   console.log("hi!!!")
+//   window.addEventListener('deviceorientation',(event) => {
+//     // Expose each orientation angle in a more readable way
+//     rotation_degrees = event.alpha;
+//     frontToBack_degrees = event.beta;
+//     leftToRight_degrees = event.gamma;
     
-    // Update velocity according to how tilted the phone is
-    // Since phones are narrower than they are long, double the increase to the x velocity
-    vx = vx + leftToRight_degrees * updateRate*2; 
-    vy = vy + frontToBack_degrees * updateRate;
+//     // Update velocity according to how tilted the phone is
+//     // Since phones are narrower than they are long, double the increase to the x velocity
+//     vx = vx + leftToRight_degrees * updateRate*2; 
+//     vy = vy + frontToBack_degrees * updateRate;
     
-    // Update position and clip it to bounds
-    px = px + vx*.5;
-    if (px > 98 || px < 0){ 
-        px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
-        vx = 0;
-    }
+//     // Update position and clip it to bounds
+//     px = px + vx*.5;
+//     if (px > 98 || px < 0){ 
+//         px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
+//         vx = 0;
+//     }
 
-    py = py + vy*.5;
-    if (py > 98 || py < 0){
-        py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
-        vy = 0;
-    }
+//     py = py + vy*.5;
+//     if (py > 98 || py < 0){
+//         py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
+//         vy = 0;
+//     }
     
-    dot = document.getElementsByClassName("indicatorDot")[0]
-    dot.setAttribute('style', "left:" + (px) + "%;" +
-                                  "top:" + (py) + "%;");
+//     dot = document.getElementsByClassName("indicatorDot")[0]
+//     dot.setAttribute('style', "left:" + (px) + "%;" +
+//                                   "top:" + (py) + "%;");
     
-  });
-};
+//   });
+// };
 
-const loadDeviceSensor = () => {
-  if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    // Handle iOS 13+ devices.
-    DeviceMotionEvent.requestPermission()
-      .then((state) => {
-        if (state === 'granted') {
-          window.addEventListener('devicemotion', handleOrientation);
-        } else {
-          console.error('Request to access the orientation was rejected');
-        }
-      })
-      .catch(console.error);
-  } else {
-    // Handle regular non iOS 13+ devices.
-    window.addEventListener('devicemotion', handleOrientation);
-  }
-};
+// const loadDeviceSensor = () => {
+//   if (typeof DeviceMotionEvent.requestPermission === 'function') {
+//     // Handle iOS 13+ devices.
+//     DeviceMotionEvent.requestPermission()
+//       .then((state) => {
+//         if (state === 'granted') {
+//           window.addEventListener('devicemotion', handleOrientation);
+//         } else {
+//           console.error('Request to access the orientation was rejected');
+//         }
+//       })
+//       .catch(console.error);
+//   } else {
+//     // Handle regular non iOS 13+ devices.
+//     window.addEventListener('devicemotion', handleOrientation);
+//   }
+// };
 
 const detectFaces = async () => {
   const prediction = await model.estimateFaces(video, false);
-
-  // console.log(prediction);
 
   // draw the video first
   ctx.drawImage(video, 0, 0, 300, 200);
 
   prediction.forEach((pred) => {
+
+    // const strength = 10;
+    // const amountOfSmoothingValues = 4;
+    // let pastValues = Array(amountOfSmoothingValues).fill(0).map(val => [val, val]); 
+    // const x = pred.topLeft[0];
+    // const y = pred.topLeft[1];
+    // const width = pred.bottomRight[0] - x;
+    // const height = pred.bottomRight[1] - y;
+    // // get coordinates ranging from 0 to 1, and flipped horizontally
+    // const middle = [2 * (x + width / 2) / video.videoWidth - 1, -2 * (y + height / 2) / video.videoHeight + 1];
+    // pastValues = [...pastValues.slice(1, amountOfSmoothingValues), middle];
+    // const average = pastValues
+    //     .reduce((sum, [x, y]) => [sum[0] + x, sum[1] + y], [0, 0])
+    //     .map(v => v / amountOfSmoothingValues);
+
+    // text.style.transform = `translate3D(${strength * average[0]}vw, ${strength * average[1]}vh, -45vw)`;
+    // text.style.translateZ = '-45vw';
+    // text.style.transformStyle = 'preserve-3d';
+    // ^^^^
     
     // draw the rectangle enclosing the face
     ctx.beginPath();
@@ -92,33 +109,48 @@ const detectFaces = async () => {
     // the last two arguments are width and height
     // since blazeface returned only the coordinates, 
     // we can find the width and height by subtracting them.
+    width = pred.bottomRight[0] - pred.topLeft[0];
     ctx.rect(
       pred.topLeft[0],
       pred.topLeft[1],
-      pred.bottomRight[0] - pred.topLeft[0],
+      width,
       pred.bottomRight[1] - pred.topLeft[1]
     );
     ctx.stroke();
     
     // drawing small rectangles for the face landmarks
-    // ctx.fillStyle = "red";
-    // pred.landmarks.forEach((landmark) => {
-    //   ctx.fillRect(landmark[0], landmark[1], 5, 5);
-    // });
+    ctx.fillStyle = "red";
+    pred.landmarks.forEach((landmark) => {
+      ctx.fillRect(landmark[0], landmark[1], 5, 5);
+    });
 
-    // console.log(pred.topLeft[0], pred.topLeft[1]);
+    const midpoint = (width)/2;
+    const noseX = pred.landmarks[2][0];
+
+    // perspectiveShift = (noseX-width - midpoint) < 20 ? 20 : (noseX-width - midpoint);
+    perspectiveShift = (noseX-width)*10;
+
+    // console.log(perspectiveShift)
+    // console.log(pred);
     text.style.height = windowHeight + 'px';
     text.style.width = windowWidth*0.8 - 300 + 'px';
     text.style.right = pred.topLeft[0] + 'px';
     text.style.top = pred.topLeft[1] + 'px';
+
+    if (document.getElementById("checkbox-toggle").checked) {
+      text.style.transform = 'perspective(' + perspectiveShift + 'px) rotateY(' + 10 + 'deg)';
+    } else {
+      text.style.transform = 'perspective(0px) rotateY(0deg)';
+    }
     
   });
 };
 
-loadDeviceSensor();
+// loadDeviceSensor();
 setupCamera();
 video.addEventListener("loadeddata", async () => {
   model = await blazeface.load();
+
   // call detect faces every 100 milliseconds or 10 times every second
   setInterval(detectFaces, 100);
 });
